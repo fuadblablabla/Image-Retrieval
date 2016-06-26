ae080a8288c7f6382692410302fdf08086d2c85b
fuadblablabla <mfuadab@gmail.com>
2016-06-26 23:12:29 +0700

    fix quantization rgp, and search

--- src/imageretrieval/ConnectDatabase.java
+++ src/imageretrieval/ConnectDatabase.java
@@ -8,8 +8,13 @@ package imageretrieval;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  *
@@ -17,8 +22,7 @@ import java.sql.Statement;
  */
 public class ConnectDatabase {
 
-    private Connection connection = null;
-    private Statement statement = null;
+    private Connection connection = null;    
 
     public void loadDriver() {
         try {
@@ -57,33 +61,103 @@ public class ConnectDatabase {
         }
     }
 
-    public void insert(Warna warna) {
+    public void insert(Warna warna, ArrayList<Integer> level) {
         try {
             connected();
             System.out.println("URL: " + warna.getUrl());
-            String sql = "Insert into warna (black,white,red,lime,blue,yellow,cyan,magenta,silver,gray,maroon,olive,green,purple,teal,navy,url)values(" + warna.getC1() + "," 
-                    + warna.getC2() + "," 
-                    + warna.getC3() + "," 
-                    + warna.getC4() + "," 
-                    + warna.getC5() + "," 
-                    + warna.getC6() + "," 
-                    + warna.getC7() + "," 
-                    + warna.getC8() + "," 
-                    + warna.getC9() + "," 
-                    + warna.getC10() + "," 
-                    + warna.getC11() + "," 
-                    + warna.getC12() + "," 
-                    + warna.getC13() + "," 
-                    + warna.getC14() + "," 
-                    + warna.getC15() + "," 
-                    + warna.getC16() + ", '" 
-                    + warna.getUrl() + "')";
-            //statement.executeQuery(sql);
+            String sql = "Insert into color_bin_r (level_0,level_16,level_32,level_48,level_64,level_80,level_96,level_112,level_128,level_144,level_160,level_176,level_192,level_208,level_224,level_240,level_256,url)"
+                    + "values(" + warna.getColorBinR().get(level.get(0))
+                    + "," + warna.getColorBinR().get(level.get(1))
+                    + "," + warna.getColorBinR().get(level.get(2))
+                    + "," + warna.getColorBinR().get(level.get(3))
+                    + "," + warna.getColorBinR().get(level.get(4))
+                    + "," + warna.getColorBinR().get(level.get(5))
+                    + "," + warna.getColorBinR().get(level.get(6))
+                    + "," + warna.getColorBinR().get(level.get(7))
+                    + "," + warna.getColorBinR().get(level.get(8))
+                    + "," + warna.getColorBinR().get(level.get(9))
+                    + "," + warna.getColorBinR().get(level.get(10))
+                    + "," + warna.getColorBinR().get(level.get(11))
+                    + "," + warna.getColorBinR().get(level.get(12))
+                    + "," + warna.getColorBinR().get(level.get(13))
+                    + "," + warna.getColorBinR().get(level.get(14))
+                    + "," + warna.getColorBinR().get(level.get(15))
+                    + "," + warna.getColorBinR().get(level.get(16))
+                    + ",'" + warna.getUrl() + "')";
             System.out.println(sql);
             connection.prepareStatement(sql).executeUpdate(sql);
+
+            String sql2 = "Insert into color_bin_g (level_0,level_16,level_32,level_48,level_64,level_80,level_96,level_112,level_128,level_144,level_160,level_176,level_192,level_208,level_224,level_240,level_256,url)"
+                    + "values(" + warna.getColorBinG().get(level.get(0))
+                    + "," + warna.getColorBinG().get(level.get(1))
+                    + "," + warna.getColorBinG().get(level.get(2))
+                    + "," + warna.getColorBinG().get(level.get(3))
+                    + "," + warna.getColorBinG().get(level.get(4))
+                    + "," + warna.getColorBinG().get(level.get(5))
+                    + "," + warna.getColorBinG().get(level.get(6))
+                    + "," + warna.getColorBinG().get(level.get(7))
+                    + "," + warna.getColorBinG().get(level.get(8))
+                    + "," + warna.getColorBinG().get(level.get(9))
+                    + "," + warna.getColorBinG().get(level.get(10))
+                    + "," + warna.getColorBinG().get(level.get(11))
+                    + "," + warna.getColorBinG().get(level.get(12))
+                    + "," + warna.getColorBinG().get(level.get(13))
+                    + "," + warna.getColorBinG().get(level.get(14))
+                    + "," + warna.getColorBinG().get(level.get(15))
+                    + "," + warna.getColorBinG().get(level.get(16))
+                    + ",'" + warna.getUrl() + "')";
+            System.out.println(sql2);
+            connection.prepareStatement(sql2).executeUpdate(sql2);
+
+            String sql3 = "Insert into color_bin_b (level_0,level_16,level_32,level_48,level_64,level_80,level_96,level_112,level_128,level_144,level_160,level_176,level_192,level_208,level_224,level_240,level_256,url)"
+                    + "values(" + warna.getColorBinB().get(level.get(0))
+                    + "," + warna.getColorBinB().get(level.get(1))
+                    + "," + warna.getColorBinB().get(level.get(2))
+                    + "," + warna.getColorBinB().get(level.get(3))
+                    + "," + warna.getColorBinB().get(level.get(4))
+                    + "," + warna.getColorBinB().get(level.get(5))
+                    + "," + warna.getColorBinB().get(level.get(6))
+                    + "," + warna.getColorBinB().get(level.get(7))
+                    + "," + warna.getColorBinB().get(level.get(8))
+                    + "," + warna.getColorBinB().get(level.get(9))
+                    + "," + warna.getColorBinB().get(level.get(10))
+                    + "," + warna.getColorBinB().get(level.get(11))
+                    + "," + warna.getColorBinB().get(level.get(12))
+                    + "," + warna.getColorBinB().get(level.get(13))
+                    + "," + warna.getColorBinB().get(level.get(14))
+                    + "," + warna.getColorBinB().get(level.get(15))
+                    + "," + warna.getColorBinB().get(level.get(16))
+                    + ",'" + warna.getUrl() + "')";
+            System.out.println(sql3);
+            connection.prepareStatement(sql3).executeUpdate(sql3);
             closed();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public ObservableList<Warna> listColorBinR(ArrayList<Integer> level) {
+        try {
+            connected();
+            ObservableList<Warna> list = FXCollections.observableArrayList();
+            String sql = "Select * from color_bin_r";
+            ResultSet rs = connection.prepareStatement(sql).executeQuery(sql);
+            while (rs.next()) {
+                Warna warna = new Warna();
+                HashMap<Integer, Double> tempList = new HashMap<>();
+                warna.setId(rs.getInt(1));
+                for (int i = 2; i <= 18; i++) {
+                    tempList.put(level.get(i-2), rs.getDouble(i));
+                }
+                warna.setColorBinR(tempList);
+                list.add(warna);
+            }
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            closed();
         }
     }
 }
--- src/imageretrieval/ImageRetrievalController.java
+++ src/imageretrieval/ImageRetrievalController.java
@@ -11,12 +11,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -42,16 +47,16 @@ public class ImageRetrievalController implements Initializable {
     @FXML
     private Label label;
 
-    Parent root;
-    Image img;
-    File file;
-    static private Vector<Vector<Double>> state;
-    static private Vector<Double> item;
-    static private Vector<Double> ecludianDistance = new Vector<Double>();
-    static private int maxLen = 3;
-    static private int banyakData;
-    static private Vector<Integer> metaData = new Vector<Integer>();
-    ConnectDatabase connectDatabase;
+    private Parent root;
+    private Image img;
+    private File file;    
+    private ConnectDatabase connectDatabase;
+
+    private HashMap<Integer, Double> colorBinR;
+    private HashMap<Integer, Double> colorBinG;
+    private HashMap<Integer, Double> colorBinB;
+    private int jumlahPixel;
+    private ArrayList<Integer> level = new ArrayList<Integer>();
 
     @FXML
     private Button button;
@@ -61,6 +66,8 @@ public class ImageRetrievalController implements Initializable {
     private Button buttonProcess;
     @FXML
     private Button buttonSave;
+    @FXML
+    private Button buttonSearch;
 
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException {
@@ -84,42 +91,50 @@ public class ImageRetrievalController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO        
-        state = new Vector<Vector<Double>>();
-        FileReader fr = null;
-        try {
-            fr = new FileReader("d:/KULIAH/LJ/KNN/src/knn/dataWarna.txt");
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(ImageRetrievalController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        BufferedReader br = new BufferedReader(fr);
-
-        String[] strArr = null;
-        String str = null;
-        try {
-            while ((str = br.readLine()) != null) {
-                item = new Vector<Double>();
-                strArr = str.split("\t");
-                for (int i = 0; i < maxLen; i++) {
-                    item.add(i, Double.valueOf(strArr[i]));
-                }
-                state.add(item);
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(ImageRetrievalController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println(state);
-        banyakData = state.size();
-
-        connectDatabase = new ConnectDatabase();        
+        // TODO                
+        connectDatabase = new ConnectDatabase();
+        level.add(0);
+        level.add(16);
+        level.add(32);
+        level.add(48);
+        level.add(64);
+        level.add(80);
+        level.add(96);
+        level.add(112);
+        level.add(128);
+        level.add(144);
+        level.add(160);
+        level.add(176);
+        level.add(192);
+        level.add(208);
+        level.add(224);
+        level.add(240);
+        level.add(256);
     }
 
     private void emptyMetaData() {
-        metaData = new Vector<Integer>();
-        for (int i = 0; i < banyakData; i++) {
-            metaData.add(0);
+        colorBinR = new HashMap<Integer, Double>();
+        colorBinG = new HashMap<Integer, Double>();
+        colorBinB = new HashMap<Integer, Double>();
+        jumlahPixel = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < level.size(); j++) {
+                switch (i) {
+                    case 0:
+                        colorBinR.put(level.get(j), 0.0);
+                        break;
+                    case 1:
+                        colorBinG.put(level.get(j), 0.0);
+                        break;
+                    case 2:
+                        colorBinB.put(level.get(j), 0.0);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
-        System.out.println("metaData " + metaData);
     }
 
     @FXML
@@ -138,70 +153,76 @@ public class ImageRetrievalController implements Initializable {
         PixelWriter writer = dest.getPixelWriter();
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
+                jumlahPixel = jumlahPixel + 1;
                 Color color = reader.getColor(i, j);
                 double r = color.getRed();
                 double g = color.getGreen();
                 double b = color.getBlue();
                 double a = color.getOpacity();
 
-                double[] T1 = new double[maxLen];
-                T1[0] = r * 255;
-                T1[1] = g * 255;
-                T1[2] = b * 255;
-                ecludianDistance = new Vector<Double>();
+                int[] T1 = new int[3];
+                T1[0] = (int) (r * 255);
+                T1[1] = (int) (g * 255);
+                T1[2] = (int) (b * 255);
 
-                //System.out.println(r*255);
-                for (int k = 0; k < banyakData; k++) {
-                    double[] T = new double[maxLen];
-                    for (int l = 0; l < maxLen; l++) {
-                        double number = state.get(k).get(l);
-                        T[l] = number - T1[l];
-                    }
-                    double D = 0;
-                    for (int l = 0; l < maxLen; l++) {
-                        D = D + (T[l] * T[l]);
-                    }
-                    ecludianDistance.add(Math.sqrt(D));
-                }
+                //int xg = (int) ((T1[0] + T1[1] + T1[2]) /3);
+                //int xk = 16 * (int) (xg / 16);
+                int newR = 16 * (int) (T1[0] / 16);
+                int newG = 16 * (int) (T1[1] / 16);
+                int newB = 16 * (int) (T1[2] / 16);
 
-                double nearestColor = Collections.min(ecludianDistance);
-                int nearestColorIndex = 100;
-                for (int k = 0; k < banyakData; k++) {
-                    if (ecludianDistance.get(k) == nearestColor) {
-                        nearestColorIndex = k;
-                        break;
-                    }
-                }
-                metaData.set(nearestColorIndex, metaData.get(nearestColorIndex) + 1);
+                /*System.out.println("r: " + T1[0] + " g: " + T1[1] + " b: " + T1[2]);
+                System.out.println("xg " + xg);
+                System.out.println("xk " + xk);*/
+                //colorBin.put(xk, colorBin.get(xk)+1);
+                colorBinR.put(newR, colorBinR.get(newR) + 1);
+                colorBinG.put(newG, colorBinG.get(newG) + 1);
+                colorBinB.put(newB, colorBinB.get(newB) + 1);
 
-                Color newColor = Color.color(state.get(nearestColorIndex).get(0) / 255, state.get(nearestColorIndex).get(1) / 255, state.get(nearestColorIndex).get(2) / 255, a);
+                /*Color newColor = Color.color(Double.valueOf(xk) / 255, Double.valueOf(xk) / 255, Double.valueOf(xk) / 255, a);*/
+                Color newColor = Color.color(Double.valueOf(newR) / 255, Double.valueOf(newG) / 255, Double.valueOf(newB) / 255, a);
                 writer.setColor(i, j, newColor);
             }
-            imageSource.setImage(dest);
         }
-        System.out.println("meta data: " + metaData);
-        Warna warna = new Warna(metaData.get(0),
-                metaData.get(1),
-                metaData.get(2),
-                metaData.get(3),
-                metaData.get(4),
-                metaData.get(5),
-                metaData.get(6),
-                metaData.get(7),
-                metaData.get(8),
-                metaData.get(9),
-                metaData.get(10),
-                metaData.get(11),
-                metaData.get(12),
-                metaData.get(13),
-                metaData.get(14),
-                metaData.get(15),
-                file.toURI().toURL().toString());
-        connectDatabase.insert(warna);
+        imageSource.setImage(dest);
+        
+        System.out.println("jumlah pixel: " + jumlahPixel);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < level.size(); j++) {
+                switch (i) {
+                    case 0:
+                        colorBinR.put(level.get(j), colorBinR.get(level.get(j))/jumlahPixel);
+                        break;
+                    case 1:
+                        colorBinG.put(level.get(j), colorBinG.get(level.get(j))/jumlahPixel);
+                        break;
+                    case 2:
+                        colorBinB.put(level.get(j), colorBinB.get(level.get(j))/jumlahPixel);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        System.out.println("color bin red: " + colorBinR);
+        System.out.println("color bin green: " + colorBinG);
+        System.out.println("color bin blue: " + colorBinB);        
     }
 
     @FXML
-    private void saveImage(ActionEvent event) {
+    private void saveImage(ActionEvent event) throws MalformedURLException {
+        Warna warna = new Warna(colorBinR, colorBinG, colorBinB, 
+                file.toURI().toURL().toString());
+        connectDatabase.insert(warna, level);
+    }
+
+    @FXML
+    private void searchImage(ActionEvent event) {
+        ObservableList<Warna> listColorBinR = FXCollections.observableArrayList();
+        listColorBinR = connectDatabase.listColorBinR(level);
+        System.out.println("list color bin r: " + listColorBinR.get(0).getColorBinR());
     }
 
 }
